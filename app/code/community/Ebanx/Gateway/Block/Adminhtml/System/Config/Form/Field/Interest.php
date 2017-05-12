bca371df0da85a5885963285a21f36f011e2664e
Guilherme Coelho Piovesan <guilherme.coelho@ebanx.com>
2017-05-12 00:25:48 -0300

    Create interest rates field on admin

---
+++ app/code/community/Ebanx/Gateway/Block/Adminhtml/System/Config/Form/Field/Interest.php
@@ -0,0 +1,70 @@
+<?php
+class Ebanx_Gateway_Block_Adminhtml_System_Config_Form_Field_Interest
+    extends Mage_Adminhtml_Block_System_Config_Form_Field_Array_Abstract {
+    public function __construct() {
+        $this->addColumn('installments', array(
+            'label' => Mage::helper('gateway')->__('Up To'),
+            'style' => 'width:120px',
+        ));
+
+        $this->addColumn('interest', array(
+            'label' => Mage::helper('gateway')->__('Interest Rate'),
+            'style' => 'width:120px',
+        ));
+
+        $this->_addAfter = false;
+
+        parent::__construct();
+    }
+
+    /**
+     * Render array cell for prototypeJS template
+     *
+     * @param string $columnName
+     * @return string
+     */
+    protected function _renderCellTemplate($columnName) {
+        if (empty($this->_columns[$columnName])) {
+            throw new Exception('Wrong column name specified.');
+        }
+
+        $column     = $this->_columns[$columnName];
+        $elementName  = $this->getElement()->getName() . '[#{_id}][' . $columnName . ']';
+        $extraParams = ' data-value="#{' . $columnName . '}" ' .
+            (isset($column['style']) ? ' style="'.$column['style'] . '"' : '');
+
+        if ($columnName == 'installments') {
+            return $this->getInstallmentsSelectHtml($elementName, $extraParams);
+        }
+        return parent::_renderCellTemplate($columnName);
+    }
+
+    /**
+     * @param $name
+     * @param $extraParams
+     * @return string
+     */
+    public function getInstallmentsSelectHtml($name, $extraParams) {
+        $select = $this->getLayout()->createBlock('adminhtml/html_select')
+            ->setName($name)
+            ->setClass('select-installments')
+            ->setExtraParams($extraParams)
+            ->setOptions(Mage::getSingleton('gateway/source_instalment')->toOptionArray());
+
+        return $select->getHtml();
+    }
+
+    /**
+     * Render block HTML
+     *
+     * @return string
+     */
+    protected function _toHtml() {
+        $html = parent::_toHtml();
+        $html .= Mage::helper('adminhtml/js')->getScript(
+            "$$('.select-installments').each(function(el){ el.value = el.readAttribute('data-value'); });\n"
+        );
+
+        return $html;
+    }
+}
