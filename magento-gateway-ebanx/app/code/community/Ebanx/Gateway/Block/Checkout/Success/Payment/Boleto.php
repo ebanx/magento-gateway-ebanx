f97c33242e6117fc9d3d29f3473a9f428d1efe38
Guilherme Coelho Piovesan <guilherme.coelho@ebanx.com>
2017-05-29 00:33:00 -0300

    Update extends class on Boleto success

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Checkout/Success/Payment/Boleto.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Checkout/Success/Payment/Boleto.php
@@ -1,19 +1,7 @@
 <?php
-class Ebanx_Gateway_Block_Checkout_Success_Payment_Boleto extends Ebanx_Gateway_Block_Checkout_Success_Payment {
+class Ebanx_Gateway_Block_Checkout_Success_Payment_Boleto extends Ebanx_Gateway_Block_Checkout_Success_CashPayment {
     protected function _construct() {
         parent::_construct();
-    }
-    
-    public function getEbanxPaymentHash() {
-        return $this->getOrder()->getPayment()->getEbanxPaymentHash();
-    }
-    
-    public function getEbanxDueDate() {
-        return Mage::helper('core')->formatDate($this->getOrder()->getPayment()->getEbanxDueDate(), Mage_Core_Model_Locale::FORMAT_TYPE_FULL, false); // Sunday, May 15, 2016 3:05:15 PM America/New_York
-    }
-
-	public function getEbanxBarCode() {
-        return $this->getOrder()->getPayment()->getEbanxBarCode();
     }
     
 	public function getEbanxBarCodeFormated() {
@@ -30,18 +18,4 @@ class Ebanx_Gateway_Block_Checkout_Success_Payment_Boleto extends Ebanx_Gateway_
 			'boleto8' => '<span>' . substr($code, 33, 14) . '</span>',
 		);
 	}
-
-	public function getEbanxUrl() {
-        return Mage::getSingleton('ebanx/api')->getEbanxUrl() . self::getEbanxPaymentHash();
-    }
-
-	public function getEbanxUrlPdf() {
-        return self::getEbanxUrl() . '&format=pdf';
-    }
-
-	public function getEbanxUrlPrint() {
-        return self::getEbanxUrl() . '&format=print';
-    }
-    
-    
 }
