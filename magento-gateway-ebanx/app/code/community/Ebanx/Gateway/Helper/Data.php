90c15e69e3ab6d79c7f0f389bbd4958348b38528
Guilherme Coelho Piovesan <guilherme.coelho@ebanx.com>
2017-05-29 15:34:28 -0300

    Update success page template

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Helper/Data.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Helper/Data.php
@@ -28,4 +28,25 @@ class Ebanx_Gateway_Helper_Data extends Mage_Core_Helper_Abstract {
         $dueDate = new Zend_Date(Mage::getModel('core/date')->timestamp());
         return $dueDate->addDay($this->getDueDateDays());
     }
+
+    public function getSuccessTemplate(){
+        $orderId = Mage::app()->getRequest()->getParam('order_id');
+        $order = Mage::getModel("sales/order")->load($orderId);
+
+        Mage::log($order->getPayment()->getMethodInstance()->getCode(), null, 'benjamin-request.log', true);
+        $paymentMethod = $order->getPayment()->getMethodInstance()->getCode();
+
+        switch ($paymentMethod) {
+            case 'ebanx_baloto':
+                return 'ebanx/checkout/success/baloto.phtml';
+                break;
+            case 'ebanx_boleto':
+                return 'ebanx/checkout/success/boleto.phtml';
+                break;
+            
+            default:
+                return null;
+                break;
+        }
+    }
 }
