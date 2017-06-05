f067ef93f8ac0f23813c9b18657ad88f0b6f5218
Guilherme Coelho Piovesan <guilherme.coelho@ebanx.com>
2017-06-05 15:48:33 -0300

    Update Baloto URLs on success page

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Checkout/Success/Payment/Baloto.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Checkout/Success/Payment/Baloto.php
@@ -3,4 +3,8 @@ class Ebanx_Gateway_Block_Checkout_Success_Payment_Baloto extends Ebanx_Gateway_
     protected function _construct() {
         parent::_construct();
     }
+
+	public function getEbanxUrl() {
+        return parent::getEbanxUrl() . 'baloto/execute';
+    }
 }
