dce9b86e2074cac20ed881698dd039b8405762a7
Guilherme Pressutto <gpressutto5@gmail.com>
2017-06-05 17:17:35 -0300

    Added blank lines

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Model/Mexico/Oxxo.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Model/Mexico/Oxxo.php
@@ -2,9 +2,12 @@
 class Ebanx_Gateway_Model_Mexico_Oxxo extends Ebanx_Gateway_Model_Payment
 {
     protected $gateway;
+
     protected $_code = 'ebanx_oxxo';
+
     protected $_formBlockType = 'ebanx/form_oxxo';
     protected $_infoBlockType = 'ebanx/info_oxxo';
+
     public function __construct()
     {
         parent::__construct();
