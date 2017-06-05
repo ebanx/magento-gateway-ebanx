1788df9b2bd463a01b3c219e5d363f2a0119e006
Cezar Luiz <cezarluiz.c@gmail.com>
2017-06-05 18:30:40 -0300

    Added method to get the banks list of Colombia

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Form/Pse.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Form/Pse.php
@@ -6,4 +6,29 @@ class Ebanx_Gateway_Block_Form_Pse extends Mage_Payment_Block_Form
         parent::_construct();
         $this->setTemplate('ebanx/form/pse.phtml');
     }
+
+	public function getBanksList()
+	{
+		return [
+			'banco_agrario' => 'Banco Agrario',
+			'banco_av_villas' => 'Banco AV Villas',
+			'banco_bbva_colombia_s.a.' => 'Banco BBVA Colombia',
+			'banco_caja_social' => 'Banco Caja Social',
+			'banco_colpatria' => 'Banco Colpatria',
+			'banco_cooperativo_coopcentral' => 'Banco Cooperativo Coopcentral',
+			'banco_corpbanca_s.a' => 'Banco CorpBanca Colombia',
+			'banco_davivienda' => 'Banco Davivienda',
+			'banco_de_bogota' => 'Banco de Bogotá',
+			'banco_de_occidente' => 'Banco de Occidente',
+			'banco_falabella_' => 'Banco Falabella',
+			'banco_gnb_sudameris' => 'Banco GNB Sudameris',
+			'banco_pichincha_s.a.' => 'Banco Pichincha',
+			'banco_popular' => 'Banco Popular',
+			'banco_procredit' => 'Banco ProCredit',
+			'bancolombia' => 'Bancolombia',
+			'bancoomeva_s.a.' => 'Bancoomeva',
+			'citibank_' => 'Citibank',
+			'helm_bank_s.a.' => 'Helm Bank'
+		];
+	}
 }
