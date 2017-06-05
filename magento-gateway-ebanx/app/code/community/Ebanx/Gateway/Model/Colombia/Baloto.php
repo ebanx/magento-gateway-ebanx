3bd2cde68a88c41336a2bf81d601e224aa54cac5
Guilherme Coelho Piovesan <guilherme.coelho@ebanx.com>
2017-06-05 10:47:56 -0300

    Update Baloto path

---
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Model/Colombia/Baloto.php
@@ -0,0 +1,24 @@
+<?php
+
+class Ebanx_Gateway_Model_Colombia_Baloto extends Ebanx_Gateway_Model_Payment
+{
+	protected $gateway;
+
+	protected $_code = 'ebanx_baloto';
+
+	protected $_formBlockType = 'ebanx/form_baloto';
+	protected $_infoBlockType = 'ebanx/info_baloto';
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->gateway = $this->ebanx->baloto();
+	}
+
+	public function persistPayment()
+	{
+		parent::persistPayment();
+		$this->payment->setEbanxBarCode($this->result['payment']['baloto_barcode']);
+	}
+}
