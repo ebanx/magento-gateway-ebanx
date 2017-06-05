563617a2895b6e0ffcccdc5c754a45d782e4c0f1
Cezar Luiz <cezarluiz.c@gmail.com>
2017-06-05 16:59:40 -0300

    Created gateway

---
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Model/Brazil/Wallet.php
@@ -0,0 +1,17 @@
+<?php
+class Ebanx_Gateway_Model_Brazil_Wallet extends Ebanx_Gateway_Model_Payment
+{
+	protected $gateway;
+
+	protected $_code = 'ebanx_wallet';
+
+	protected $_formBlockType = 'ebanx/form_wallet';
+	protected $_infoBlockType = 'ebanx/info_wallet';
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->gateway = $this->ebanx->ebanxAccount();
+	}
+}
