0c076df392c8a0ea2010204bf00fd618120a15de
Cezar Luiz <cezarluiz.c@gmail.com>
2017-06-07 14:57:26 -0300

    Updated payment notification code

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Model/Observer.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Model/Observer.php
@@ -8,27 +8,36 @@ class Ebanx_Gateway_Model_Observer
 
 		if (!$this->isEbanxPaymentRequest($request)) return;
 
-		// TOOD: Make a Benjamin query to find the payment by hash
-		$payment = [
-			'payment' => [
-				'status' => 'PE'
-			]
-		];
-
 		$helper = Mage::helper('ebanx');
+		$hash   = $request->hash;
+		$order  = $helper->getOrderByHash($hash);
 
-		$hash = $request->hash;
-		$order = $helper->getOrderByHash($hash);
-		$ebanxStatus = $payment['payment']['status'];
-		$status = $helper->getEbanxMagentoOrder($ebanxStatus);
+		try {
+			$api     = Mage::getSingleton('ebanx/api')->ebanx();
+			$mode    = $order->getEbanxEnvironment() === 'sandbox' ? true: false;
+			$payment = $api->paymentInfo()->findByHash($hash, $mode);
 
-		// Updating the order status
-		$order->setState($status, true);
+			$ebanxStatus = $payment['payment']['status'];
 
-		// Adding an order note
-		$order->addStatusHistoryComment(__('EBANX: The payment has been updated to: %s.', $helper->getTranslatedOrderStatus($ebanxStatus)));
+			$status = $helper->getEbanxMagentoOrder($ebanxStatus);
 
-		$order->save();
+			// Updating the order status
+			$order->setData('status', $status);
+
+			// Adding an order note
+			$order->addStatusHistoryComment(__('EBANX: The payment has been updated to: %s.', $helper->getTranslatedOrderStatus($ebanxStatus)));
+
+			$order->save();
+
+			Mage::log(print_r($payment, true), null, 'ebanx_order_notification.log', true);
+		}
+		catch (Exception $e) {
+			$order->addStatusHistoryComment(__('EBANX: We could not update the order status. Error message: %s.', $e->getMessage()));
+
+			Mage::log($e->getMessage(), null, 'ebanx_error.log', true);
+
+			// Mage::throwException($e->getMessage());
+		}
 	}
 
 	private function isEbanxPaymentRequest($request)
