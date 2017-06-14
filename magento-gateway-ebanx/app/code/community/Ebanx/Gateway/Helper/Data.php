c8f1fb0d54ae09f0e83251c0dbc6701ebabe7aba
Cezar Luiz <cezarluiz.c@gmail.com>
2017-06-14 17:42:08 -0300

    Showing compliance fields when the settings are empty

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Helper/Data.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Helper/Data.php
@@ -78,4 +78,63 @@ class Ebanx_Gateway_Helper_Data extends Mage_Core_Helper_Abstract
 
 		return $banks[strtolower($bankCode)];
 	}
+
+	public function hasComplianceFieldFromSettings($code)
+	{
+		$methodsToFields = [
+			'ebanx_boleto' => ['cpf_field', 'cnpj_field'],
+			'ebanx_tef' => ['cpf_field', 'cnpj_field'],
+			'ebanx_wallet' => ['cpf_field', 'cnpj_field'],
+			'ebanx_cc_br' => ['cpf_field', 'cnpj_field'],
+			'ebanx_sencillito' => ['rut_field'],
+			'ebanx_servipag' => ['rut_field'],
+			'ebanx_baloto' => ['dni_field'],
+			'ebanx_pse' => ['dni_field'],
+			'ebanx_oxxo' => [],
+			'ebanx_cc_mx' => [],
+			'ebanx_dc_mx' => [],
+			'ebanx_pagoefectivo' => [],
+			'ebanx_safetypay' => []
+		];
+
+		$fields = $methodsToFields[$code];
+
+		foreach ($fields as $field) {
+			if (!Mage::getStoreConfig('payment/ebanx_settings/' . $field)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public function getBrazilianDocumentLabel()
+	{
+		$label = [];
+		$taxes = explode(',', Mage::getStoreConfig('payment/ebanx_settings/brazil_taxes'));
+
+		return strtoupper(implode(' / ', $taxes));
+	}
+
+	public function getLabelForComplianceField($code)
+	{
+		switch ($code) {
+			case 'ebanx_boleto':
+			case 'ebanx_tef':
+			case 'ebanx_wallet':
+			case 'ebanx_cc_br':
+				return $this->getBrazilianDocumentLabel();
+
+			case 'ebanx_sencillito':
+			case 'ebanx_servipag':
+				return $this->__('RUT Document');
+
+			case 'ebanx_baloto':
+			case 'ebanx_pse':
+				return $this->__('DNI Document');
+
+			default:
+				return $this->__('Document Number');
+		}
+	}
 }
