d48ba4f05a9be75f1e606f360fc52165ed7cb3c9
Cezar Luiz <cezarluiz.c@gmail.com>
2017-06-15 16:14:50 -0300

    Splitting address in street and number of the house

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Helper/Data.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Helper/Data.php
@@ -251,4 +251,36 @@ class Ebanx_Gateway_Helper_Data extends Mage_Core_Helper_Abstract
 	{
 		$this->log($data, 'ebanx_error.log');
 	}
+
+	/**
+	 * Splits address in street name, house number and addition
+	 *
+	 * @param  string $address Address to be split
+	 * @return array
+	 */
+	public function split_street($address) {
+		$result = preg_match('/^([^,\-\/\#0-9]*)\s*[,\-\/\#]?\s*([0-9]+)\s*[,\-\/]?\s*([^,\-\/]*)(\s*[,\-\/]?\s*)([^,\-\/]*)$/', $address, $matches);
+		if ($result === false) {
+			throw new \RuntimeException(sprintf('Problems trying to parse address: \'%s\'', $address));
+		}
+		if ($result === 0) {
+			return array(
+				'streetName' => $address,
+				'houseNumber' => '',
+				'additionToAddress' => ''
+			);
+		}
+		$street_name = $matches[1];
+		$house_number = $matches[2];
+		$addition_to_address = $matches[3] . $matches[4] . $matches[5];
+		if (empty($street_name)) {
+			$street_name = $matches[3];
+			$addition_to_address = $matches[5];
+		}
+		return array(
+			'streetName' => $street_name,
+			'houseNumber' => $house_number,
+			'additionToAddress' => $addition_to_address
+		);
+	}
 }
