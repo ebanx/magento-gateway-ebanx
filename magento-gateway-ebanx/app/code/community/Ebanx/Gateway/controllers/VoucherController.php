28d172f167c7c9f6d30c870b65e97c897cead7d7
Cezar Luiz <cezarluiz.c@gmail.com>
2017-06-23 20:24:54 -0300

    Getting the voucher url by helper

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/controllers/VoucherController.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/controllers/VoucherController.php
@@ -5,10 +5,8 @@ class Ebanx_Gateway_VoucherController extends Mage_Core_Controller_Front_Action
 	public function indexAction()
 	{
 		$hash = $this->getRequest()->getParam('hash');
-		$type = $this->getRequest()->getParam('type');
 
-		$helper = Mage::helper('ebanx');
-		$url = $helper->getEbanxUrl() . $type . "?hash={$hash}&format=basic";
+		$url = Mage::helper('ebanx')->getVoucherUrlByHash($hash);
 
 		if (!in_array('curl', get_loaded_extensions())) {
 			echo file_get_contents($url);
