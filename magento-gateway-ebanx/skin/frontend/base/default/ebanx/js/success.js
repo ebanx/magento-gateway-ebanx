d89ba57d1d870a96b7908c55977b5dbc3e3e06d3
Cezar Luiz <cezarluiz.c@gmail.com>
2017-06-23 20:20:04 -0300

    Created feedback to customer when it copy the boleto code

--- magento-gateway-ebanx/skin/frontend/base/default/ebanx/js/success.js
+++ magento-gateway-ebanx/skin/frontend/base/default/ebanx/js/success.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   var clipboard = new Clipboard('.ebanx-button--copy');
 
+  clipboard.on('success', function (e) {
+    var text = e.trigger.innerText;
+
+    e.trigger.innerText = 'Copiado!';
+
+    setTimeout(function () {
+      e.trigger.innerText = text;
+    }, 2000);
+  });
+
   // iFrame Resizer
   var iframe = document.querySelector('.ebanx-cash-payment iframe');
 
