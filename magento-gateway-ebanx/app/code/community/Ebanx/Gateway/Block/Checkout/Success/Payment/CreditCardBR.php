65b635f6c98699a9499915a69423b828351a7c61
Cezar Luiz <cezarluiz.c@gmail.com>
2017-06-23 20:24:08 -0300

    Created the currency code property

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Checkout/Success/Payment/CreditCardBR.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Checkout/Success/Payment/CreditCardBR.php
@@ -2,6 +2,8 @@
 
 class Ebanx_Gateway_Block_Checkout_Success_Payment_CreditCardBR extends Ebanx_Gateway_Block_Checkout_Success_CreditCardPayment
 {
+	private $currencyCode = 'BRL';
+
 	protected function _construct()
 	{
 		parent::_construct();
