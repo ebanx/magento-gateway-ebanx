168ffb6dc651a061594b83ca9098a0835c99def5
Cezar Luiz <cezarluiz.c@gmail.com>
2017-06-23 12:48:22 -0300

    Created and organized some methods

--- magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Checkout/Success/CashPayment.php
+++ magento-gateway-ebanx/app/code/community/Ebanx/Gateway/Block/Checkout/Success/CashPayment.php
@@ -4,24 +4,39 @@ class Ebanx_Gateway_Block_Checkout_Success_CashPayment extends Ebanx_Gateway_Blo
 {
 	protected $_order;
 
+	protected function _construct()
+	{
+		parent::_construct();
+	}
+
 	public function getEbanxDueDate()
 	{
 		return Mage::helper('core')->formatDate($this->getOrder()->getPayment()->getEbanxDueDate(), Mage_Core_Model_Locale::FORMAT_TYPE_FULL, false);
 	}
 
-	public function getEbanxUrlPdf()
-	{
-		return $this->getEbanxUrlIframe() . '&format=pdf';
-	}
-
-	public function getEbanxUrlIframe()
-	{
-		return $this->getEbanxUrl() . '?hash=' . $this->getEbanxPaymentHash();
-	}
-
 	public function getEbanxUrl()
 	{
-		return Mage::helper('ebanx')->getEbanxUrl();
+		return $this->helper->getEbanxUrl();
+	}
+
+	public function getEbanxUrlPrint($type)
+	{
+		return $this->getEbanxUrlIframe($type) . '&format=print';
+	}
+
+	public function getEbanxUrlPdf($type)
+	{
+		return $this->getEbanxUrlIframe($type) . '&format=pdf';
+	}
+
+	public function getEbanxUrlBasic($type)
+	{
+		return $this->getEbanxUrlIframe($type) . '&format=basic';
+	}
+
+	public function getEbanxUrlIframe($type = '')
+	{
+		return $this->getEbanxUrl() . $type . '?hash=' . $this->getEbanxPaymentHash();
 	}
 
 	public function getEbanxPaymentHash()
@@ -29,14 +44,11 @@ class Ebanx_Gateway_Block_Checkout_Success_CashPayment extends Ebanx_Gateway_Blo
 		return $this->getOrder()->getPayment()->getEbanxPaymentHash();
 	}
 
-	public function getEbanxUrlPrint()
+	public function getVoucherUrl($type)
 	{
-		return $this->getEbanxUrlIframe() . '&format=print';
+		return Mage::getUrl('ebanx/voucher', [
+			'hash' => $this->getEbanxPaymentHash(),
+			'type' => $type
+		]);
 	}
-
-	protected function _construct()
-	{
-		parent::_construct();
-	}
-
 }
