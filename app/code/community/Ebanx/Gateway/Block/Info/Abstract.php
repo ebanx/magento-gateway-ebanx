506153e9779e3969c9f5d884e00790a738c8489a
Cezar Luiz <cezarluiz.c@gmail.com>
2017-07-28 18:14:19 -0300

    Refactored methods for calculate the local amount

--- app/code/community/Ebanx/Gateway/Block/Info/Abstract.php
+++ app/code/community/Ebanx/Gateway/Block/Info/Abstract.php
@@ -2,32 +2,8 @@
 
 abstract class Ebanx_Gateway_Block_Info_Abstract extends Mage_Payment_Block_Info
 {
-	private function getQuoteData($key = null)
-	{
-		$quote = Mage::getModel('checkout/session')->getQuote();
-		$quoteData = $quote->getData();
-
-		if (is_string($key)) {
-			return $quoteData[$key];
-		}
-
-		return $quoteData;
-	}
-
-	public function getGrandTotal()
-	{
-		return $this->getQuoteData('grand_total');
-	}
-
 	public function getLocalAmount($currency, $formatted = true)
 	{
-		$amount = round(Mage::helper('ebanx')->getLocalAmountWithTax($currency, $this->getGrandTotal()), 2);
-
-		return $formatted ? $this->formatPriceWithLocalCurrency($currency, $amount) : $amount;
-	}
-
-	public function formatPriceWithLocalCurrency($currency, $price)
-	{
-		return Mage::app()->getLocale()->currency($currency)->toCurrency($price);
+		return Mage::helper('ebanx/amount')->getLocalAmount($currency, $formatted);
 	}
 }
