e265d333f73a6df820d46ede9cdd0ed4918a27e3
Cezar Luiz <cezarluiz.c@gmail.com>
2017-08-01 11:06:24 -0300

    Created abstract class to use right way method on payment block form

---
+++ app/code/community/Ebanx/Gateway/Block/Form/Abstract.php
@@ -0,0 +1,21 @@
+<?php
+
+abstract class Ebanx_Gateway_Block_Form_Abstract extends Mage_Payment_Block_Form
+{
+	private function getTotal()
+	{
+		return $this->getMethod()->getTotal();
+	}
+
+	private function formatPriceWithLocalCurrency($currency, $price)
+	{
+		return Mage::app()->getLocale()->currency($currency)->toCurrency($price);
+	}
+
+	public function getLocalAmount($currency, $formatted = true)
+	{
+		$amount = round(Mage::helper('ebanx')->getLocalAmountWithTax($currency, $this->getTotal()), 2);
+
+		return $formatted ? $this->formatPriceWithLocalCurrency($currency, $amount) : $amount;
+	}
+}
--- app/code/community/Ebanx/Gateway/Block/Form/Tef.php
+++ app/code/community/Ebanx/Gateway/Block/Form/Tef.php
@@ -1,6 +1,6 @@
 <?php
 
-class Ebanx_Gateway_Block_Form_Tef extends Mage_Payment_Block_Form
+class Ebanx_Gateway_Block_Form_Tef extends Ebanx_Gateway_Block_Form_Abstract
 {
 	protected function _construct()
 	{
