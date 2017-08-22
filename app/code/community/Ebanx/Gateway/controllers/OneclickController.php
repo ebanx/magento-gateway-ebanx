9c346ae80ebf94b76b6942b14d1392ec9a7dab94
Guilherme Pressutto <gpressutto5@gmail.com>
2017-08-22 17:16:54 -0300

    WIP getRealProductId

--- app/code/community/Ebanx/Gateway/controllers/OneclickController.php
+++ app/code/community/Ebanx/Gateway/controllers/OneclickController.php
@@ -2,11 +2,50 @@
 
 class Ebanx_Gateway_OneclickController extends Mage_Core_Controller_Front_Action
 {
+	/**
+	 * @var Mage_Catalog_Model_Product
+	 */
+	private $product;
+
+	/**
+	 * @var array
+	 */
+	private $request;
+
+	protected function _construct()
+	{
+		parent::_construct();
+
+		$this->request = Mage::app()->getRequest()->getPost();
+		$this->product = Mage::getModel('catalog/product')->load($this->request['product']);
+	}
+
 	public function payAction()
 	{
-		$test = Mage::app()->getRequest()->getPost();
+		$product['id'] = $this->getRealProductId();
+
 		echo '<pre>';
-		var_dump($test);
+		var_dump($product['id']);
 		echo '</pre>';
+	}
+
+	/**
+	 * If the product type isn't simple it returns the real product id
+	 *
+	 * @return string
+	 */
+	private function getRealProductId()
+	{
+		switch ($this->product->getTypeId()) {
+			case 'simple':
+				return $this->request['product'];
+			case 'bundle':
+			case 'configurable':
+			case 'downloadable':
+			case 'grouped':
+			case 'virtual':
+			default:
+				return '';
+		}
 	}
 }
