0f3b8555255d6cbbafffd1f08a82dbc853c033ff
Anderson Campanha <anderson.campanha@ebanx.com>
2017-11-06 13:39:22 -0200

    Add IOF to grand total as a tax

---
+++ app/code/community/Ebanx/Gateway/Model/Quote/Localtax.php
@@ -0,0 +1,35 @@
+<?php
+
+class Ebanx_Gateway_Model_Quote_Localtax extends Mage_Sales_Model_Quote_Address_Total_Abstract
+{
+	public function __construct()
+	{
+		$this->setCode( 'ebanx_local_tax' );
+	}
+
+	public function collect(Mage_Sales_Model_Quote_Address $address)
+	{
+		$isBrazilLocalAmount = Mage::app()->getStore()->getCurrentCurrencyCode() === 'BRL';
+		if ($isBrazilLocalAmount) {
+			$baseGrandTotal = Mage::getModel('checkout/session')->getQuote()->getData()['base_grand_total'];
+			$localTaxAmount = $baseGrandTotal*1.0038 - $baseGrandTotal;
+			$address->setEbanxLocalTaxAmount($localTaxAmount / 2);
+			$address->setGrandTotal($address->getGrandTotal() + $localTaxAmount / 2);
+		}
+	}
+
+	public function fetch(Mage_Sales_Model_Quote_Address $address) {
+		if (Mage::app()->getStore()->getCurrentCurrencyCode() === 'BRL') {
+			$amount = $address->getEbanxLocalTaxAmount();
+			$title  = Mage::helper( 'ebanx' )->__( 'IOF' );
+			if ( $amount != 0 ) {
+				$address->addTotal( array(
+					'code'  => $this->getCode(),
+					'title' => $title,
+					'value' => $amount,
+				) );
+			}
+		}
+		return $this;
+	}
+}
