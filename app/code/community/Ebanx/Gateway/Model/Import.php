9ada84cd5b050372841b6836367e37187b121eff
Anderson Campanha <anderson.campanha@ebanx.com>
2017-11-23 18:32:03 -0200

    Add blank line before return

--- app/code/community/Ebanx/Gateway/Model/Import.php
+++ app/code/community/Ebanx/Gateway/Model/Import.php
@@ -19,6 +19,7 @@ class Ebanx_Gateway_Model_Import extends Mage_Directory_Model_Currency_Import_Ab
 	protected function _convert( $currencyFrom, $currencyTo ) {
 		$this->ebanx = Mage::getSingleton('ebanx/api')->ebanx();
 		$rate = $this->ebanx->exchange()->fetchRate($currencyFrom, $currencyTo);
+
 		return $rate ?: null;
 	}
 }
