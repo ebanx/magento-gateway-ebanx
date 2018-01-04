869236de3f1885a80a3ed1270ed926a1df73789f
Anderson Campanha <anderson.campanha@ebanx.com>
2018-01-04 17:09:05 -0200

    Add argentina cc to helper file

--- app/code/community/Ebanx/Gateway/Helper/Data.php
+++ app/code/community/Ebanx/Gateway/Helper/Data.php
@@ -442,6 +442,7 @@ class Ebanx_Gateway_Helper_Data extends Mage_Core_Helper_Abstract
 			'ebanx_spei',
 			'ebanx_safetypay',
 			'ebanx_pagoefectivo',
+			'ebanx_cc_ar',
 			'ebanx_rapipago',
 			'ebanx_pagofacil',
 			'ebanx_otroscupones',
@@ -479,6 +480,7 @@ class Ebanx_Gateway_Helper_Data extends Mage_Core_Helper_Abstract
 			'ebanx_pagoefectivo' => array(),
 			'ebanx_safetypay'    => array(),
 			//Argentina
+			'ebanx_cc_ar'     => array(),
 			'ebanx_rapipago'     => array(),
 			'ebanx_pagofacil'     => array(),
 			'ebanx_otroscupones'     => array(),
