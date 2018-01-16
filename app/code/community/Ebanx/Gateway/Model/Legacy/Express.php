d0d22306ef87567be1199bd8abadf081e0e6874d
SparK-Cruz <spark.crz@gmail.com>
2018-01-16 14:56:21 -0200

    Fixing class hierarchy (thanks coelho)

--- app/code/community/Ebanx/Gateway/Model/Legacy/Express.php
+++ app/code/community/Ebanx/Gateway/Model/Legacy/Express.php
@@ -1,5 +1,5 @@
 <?php
-class Ebanx_Gateway_Model_Legacy_Express extends Ebanx_Gateway_Payment
+class Ebanx_Gateway_Model_Legacy_Express extends Mage_Payment_Model_Method_Abstract
 {
 	protected $_code = 'ebanx_express';
 	protected $_isGateway          = true;
