91179b6b7561cf027f0636d45ab5d6d11dec570f
Rafael Bruno <rafaelbruno.web@gmail.com>
2018-02-16 18:42:43 -0200

    Extract scripts to js files, and refactor input handle

--- skin/adminhtml/default/default/ebanx/js/iof-option.js
+++ skin/adminhtml/default/default/ebanx/js/iof-option.js
@@ -1,16 +1,2 @@
-document.observe("dom:loaded", function () {
-  var iofLocalAmount = document.querySelector('#payment_ebanx_settings_iof_local_amount');
-  if (!iofLocalAmount) {
-    return;
-  }
-  var confirmed;
-
-  var renderIofAlert = function (el) {
-    confirmed = confirm('You need to validate this change with EBANX, only deselecting or selecting the box will ' +
-      'not set this to your customer. Contact your EBANX Account Manager or Business Development Expert.');
-    if (!confirmed) {
-      el.target.value = 1 - el.target.value;
-    }
-  };
-  iofLocalAmount.addEventListener('change', renderIofAlert);
-});
+"use strict";document.observe("dom:loaded",function(){var e=document.querySelector("#payment_ebanx_settings_iof_local_amount");if(e){e.addEventListener("change",function(e){confirm("You need to validate this change with EBANX, only deselecting or selecting the box will not set this to your customer. Contact your EBANX Account Manager or Business Development Expert.")||(e.target.value=1-e.target.value)})}});
+//# sourceMappingURL=iof-option.js.map
