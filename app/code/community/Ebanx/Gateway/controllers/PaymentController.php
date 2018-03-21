95d85c3168965bd6bde2e59707f651e34257fb50
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2018-03-21 17:13:46 -0300

    Notification Received Logger

--- app/code/community/Ebanx/Gateway/controllers/PaymentController.php
+++ app/code/community/Ebanx/Gateway/controllers/PaymentController.php
@@ -16,6 +16,10 @@ class Ebanx_Gateway_PaymentController extends Mage_Core_Controller_Front_Action
 	public function notifyAction()
 	{
 		try {
+			Ebanx_Gateway_Log_Logger_NotificationReceived::persist(array(
+				'params' => $this->getRequest()->getParams()
+			));
+
 			$this->initialize();
 		} catch (Ebanx_Gateway_Exception $e) {
 			$this->helper->errorLog($e->getMessage());
