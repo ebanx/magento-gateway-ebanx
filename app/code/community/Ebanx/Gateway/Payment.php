6f1f60595253346cb3537f11d9159c9fef65e9a5
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2018-03-21 17:00:37 -0300

    Refund Logger

--- app/code/community/Ebanx/Gateway/Payment.php
+++ app/code/community/Ebanx/Gateway/Payment.php
@@ -148,6 +148,16 @@ abstract class Ebanx_Gateway_Payment extends Mage_Payment_Model_Method_Abstract
 	{
 		$hash = $payment->getEbanxPaymentHash();
 		$result = $this->ebanx->refund()->requestByHash($hash, $amount, $this->helper->__('Refund requested by Magento Admin Panel'));
+
+		Ebanx_Gateway_Log_Logger_Refund::persist(array(
+			'request' => array(
+				'hash' => $hash,
+				'amount' => $amount,
+				'description' => $this->helper->__('Refund requested by Magento Admin Panel')
+			),
+			'response' => $result
+		));
+
 		if($result['status'] === 'ERROR') {
 			$errorMsg = $this->helper->__('Error processing refund: '.$result['status_message'].' ('.$result['status_code'].')');
 			Mage::throwException($errorMsg);
