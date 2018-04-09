ca2524c04d39089e392115d1245b582ec27c6040
Bruno Clemente <bruno.clemente@ebanx.com>
2018-04-09 15:11:13 -0300

    Now we add to the logs the integration key

--- app/code/community/Ebanx/Gateway/controllers/LogController.php
+++ app/code/community/Ebanx/Gateway/controllers/LogController.php
@@ -8,13 +8,13 @@ class Ebanx_Gateway_LogController extends Mage_Core_Controller_Front_Action
 
 		$integration_key = $this->getRequest()->getParam('integration_key');
 
-		if (empty($integration_key) || $integration_key !== Mage::helper('ebanx/data')->getIntegrationKey()) {
+		if (empty($integration_key) || ($integration_key !== Mage::helper('ebanx/data')->getSandboxIntegrationKey() && $integration_key !== Mage::helper('ebanx/data')->getLiveIntegrationKey())) {
 			die(json_encode([]));
 		}
 
-		$res = Ebanx_Gateway_Log_Logger::fetch();
+		list($col, $res) = Ebanx_Gateway_Log_Logger::fetch($integration_key);
 
-		Ebanx_Gateway_Log_Logger::truncate();
+		Ebanx_Gateway_Log_Logger::delete($col);
 
 		echo json_encode($res);
 	}
