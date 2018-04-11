503871fd4550872b7d6bc08598d46350d04ef4c9
Pablo Suchewschy <pblwlln@gmail.com>
2018-04-11 15:27:52 -0300

    Ignoring warnings on lint

--- tests/bin/lint.sh
+++ tests/bin/lint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #current_branch=$(git branch | grep \* | sed 's/\* //g')
-php ./lib/Ebanx/vendor/squizlabs/php_codesniffer/bin/phpcs --extensions=php,phtml -v --parallel=20 .
+php ./lib/Ebanx/vendor/squizlabs/php_codesniffer/bin/phpcs --extensions=php,phtml -v -n --parallel=20 .
