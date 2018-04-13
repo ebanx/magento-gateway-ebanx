616b85ac6d2b2b901ebfa9828d87b700ef29a2b5
Pablo Suchewschy <pblwlln@gmail.com>
2018-04-13 15:33:43 -0300

    Test script reverted

--- tests/bin/test.sh
+++ tests/bin/test.sh
@@ -2,12 +2,9 @@
 
 [[ $TRAVIS_COMMIT_MESSAGE =~ ^(\[tests skip\]) ]] && echo "TESTS SKIP" && exit 0;
 
-TEST_COUNTRY=mexico
-TRAVIS_BUILD_DIR=$(pwd)
-
-# cd $TRAVIS_BUILD_DIR
-# chmod +x $(pwd)/scripts/start.sh && $(pwd)/scripts/start.sh
-# composer install
+cd $TRAVIS_BUILD_DIR
+chmod +x $(pwd)/scripts/start.sh && $(pwd)/scripts/start.sh
+composer install
 
 cd $TRAVIS_BUILD_DIR/tests
 
