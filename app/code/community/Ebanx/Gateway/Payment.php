13f9d6666c156e60631c9cfeaeab1f3e84c85540
Gabriel Frizzo <gabrielfrizzo@gmail.com>
2018-04-13 16:50:30 -0300

    Fixed last merge issues

--- app/code/community/Ebanx/Gateway/Payment.php
+++ app/code/community/Ebanx/Gateway/Payment.php
@@ -44,8 +44,7 @@ abstract class Ebanx_Gateway_Payment extends Mage_Payment_Model_Method_Abstract
             $this->payment = $this->getInfoInstance();
             $this->order = $this->payment->getOrder();
             $this->customer = Mage::getModel('sales/order')->load($this->order->getId());
-
-			$this->setupData();
+            $this->setupData();
 
             $this->transformPaymentData();
 
--- app/code/community/Ebanx/Gateway/controllers/LogController.php
+++ app/code/community/Ebanx/Gateway/controllers/LogController.php
@@ -11,13 +11,13 @@ class Ebanx_Gateway_LogController extends Mage_Core_Controller_Front_Action
 
         $integration_key = $this->getRequest()->getParam('integration_key');
 
-		if (empty($integration_key) || ($integration_key !== Mage::helper('ebanx/data')->getSandboxIntegrationKey() && $integration_key !== Mage::helper('ebanx/data')->getLiveIntegrationKey())) {
-			die(json_encode([]));
-		}
+        if (empty($integration_key) || ($integration_key !== Mage::helper('ebanx/data')->getSandboxIntegrationKey() && $integration_key !== Mage::helper('ebanx/data')->getLiveIntegrationKey())) {
+            die(json_encode([]));
+        }
 
-		list($col, $res) = Ebanx_Gateway_Log_Logger::fetch($integration_key);
+        list($col, $res) = Ebanx_Gateway_Log_Logger::fetch($integration_key);
 
-		Ebanx_Gateway_Log_Logger::delete($col);
+        Ebanx_Gateway_Log_Logger::delete($col);
 
         echo json_encode($res);
     }
