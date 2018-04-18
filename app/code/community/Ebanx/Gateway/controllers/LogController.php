59f8f24a5ea6beeff829df5dd579fee7ec534a71
Anderson Campanha <anderson.campanha@ebanx.com>
2018-04-18 17:58:34 -0300

    Adding spaces

--- app/code/community/Ebanx/Gateway/controllers/LogController.php
+++ app/code/community/Ebanx/Gateway/controllers/LogController.php
@@ -7,20 +7,20 @@ class Ebanx_Gateway_LogController extends Mage_Core_Controller_Front_Action
      */
     public function fetchAction()
     {
-        $integration_key = $this->getRequest()->getParam( 'integration_key' );
+        $integration_key = $this->getRequest()->getParam('integration_key');
 
-        if ( empty( $integration_key ) || ( $integration_key !== Mage::helper( 'ebanx/data' )->getSandboxIntegrationKey() && $integration_key !== Mage::helper( 'ebanx/data' )->getLiveIntegrationKey() ) ) {
+        if (empty($integration_key) || ($integration_key !== Mage::helper('ebanx/data')->getSandboxIntegrationKey() && $integration_key !== Mage::helper('ebanx/data')->getLiveIntegrationKey())) {
             $this->norouteAction();
 
             return;
         }
 
-        list( $col, $res ) = Ebanx_Gateway_Log_Logger::fetch( $integration_key );
+        list($col, $res) = Ebanx_Gateway_Log_Logger::fetch($integration_key);
 
-        Ebanx_Gateway_Log_Logger::delete( $col );
+        Ebanx_Gateway_Log_Logger::delete($col);
 
         $this->getResponse()
-             ->setHeader( 'Content-Type', 'application/json' )
-             ->setBody( json_encode( $res ) );
+             ->setHeader('Content-Type', 'application/json')
+             ->setBody(json_encode($res));
     }
 }
