826ed2862576676d20ca12b5a77f7bd2f76bd3c3
Anderson Campanha <anderson.campanha@ebanx.com>
2018-07-05 17:39:22 -0300

    Update js depedencies to eliminate known vulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,10 @@
 const gulp = require('gulp');
-const babelify = require('babelify');
 const babel = require('gulp-babel');
-const jshint = require('gulp-jshint');
-const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
-const cleancss = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const browserify = require('gulp-browserify');
 const eslint = require('gulp-eslint');
 const restrictedGlobals = require('eslint-restricted-globals');
 const prettier = require('gulp-prettier');
-
-// Compile SASS
-gulp.task('sass', function() {
-  return gulp.src('./src/**/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer())
-  .pipe(cleancss())
-  .pipe(rename({
-    extname: '.min.css'
-  }))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest(function(file) {
-    return file.base.replace(/\/src/, '/view');
-  }));
-});
 
 // Compile JS
 gulp.task('scripts', function() {
@@ -118,8 +95,7 @@ gulp.task('watch', function() {
   process.argv.push('--silent');
   gulp.start('default');
   gulp.watch('./src/**/*.js', ['scripts', 'lint']);
-  gulp.watch('./src/**/*.scss', ['sass']);
 });
 
 // Default Task
-gulp.task('default', ['sass', 'scripts', 'lint']);
+gulp.task('default', ['scripts', 'lint']);
