e436b081a73143fc5de911f3a798f45681ec6adf
Anderson Campanha <anderson.campanha@ebanx.com>
2018-07-23 12:37:12 -0300

    Ignore the index.php file on lint stage

--- .dev/index.php
+++ .dev/index.php
@@ -24,6 +24,8 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+// phpcs:disable
+
 if (version_compare(phpversion(), '5.3.0', '<')===true) {
     echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;">
 <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
@@ -81,3 +83,4 @@ $mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : ''
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
 
 Mage::run($mageRunCode, $mageRunType);
+// phpcs:enable
