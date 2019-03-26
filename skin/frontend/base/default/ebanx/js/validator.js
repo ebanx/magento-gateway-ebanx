68aa843241ab838fcb892a9ce004bf3cc7e99f47
Andre Starosta <andre.starosta@ebanx.com>
2019-03-26 10:50:13 -0300

    Add DNI Document for Argentine checkout

--- skin/frontend/base/default/ebanx/js/validator.js
+++ skin/frontend/base/default/ebanx/js/validator.js
@@ -1,2 +1,2 @@
-"use strict";Validation.add("brand-required"," ",function(t){return!Validation.get("IsEmpty").test(t)}),Validation.add("validate-ar-document-length","Document must have 11 digits",function(t){return 13===t.length});
+"use strict";Validation.add("brand-required"," ",function(e){return!Validation.get("IsEmpty").test(e)}),Validation.add("validate-ar-document-length","Document digit number is invalid",function(e){var t=document.querySelector("#ebanx-document-type-ebanx_cc_ar");return console.log(t.value),"ARG_DNI"===t.value?7===e.length||8===e.length:13===e.length});
 //# sourceMappingURL=validator.js.map
--- src/frontend/base/default/ebanx/js/validator.js
+++ src/frontend/base/default/ebanx/js/validator.js
@@ -4,6 +4,11 @@ Validation.add('brand-required', ' ', (v) => {
   return !Validation.get('IsEmpty').test(v);
 });
 
-Validation.add('validate-ar-document-length', 'Document must have 11 digits', (v) => {
-  return v.length === 13;
+Validation.add('validate-ar-document-length', 'Document digit number is invalid', (v) => {
+  const documentSelector = document.querySelector('#ebanx-document-type-ebanx_cc_ar');
+  if (documentSelector.value === 'ARG_DNI') {
+    return (v.length === 7 || v.length === 8);
+  } else {
+    return v.length === 13;
+  }
 });
