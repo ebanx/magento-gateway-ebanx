a346376c276b86cd3cfbf6b432cea876c8d0bc90
João Vitor Moreira <joao.moreira@ebanx.com>
2020-07-16 16:04:56 -0300

    Introduce Debitcard payment model to support multiple countries

--- app/code/community/Ebanx/Gateway/Model/Mexico/Debitcard.php
+++ app/code/community/Ebanx/Gateway/Model/Mexico/Debitcard.php
@@ -1,20 +1,13 @@
 <?php
 
-class Ebanx_Gateway_Model_Mexico_Debitcard extends Ebanx_Gateway_Payment
+use Ebanx\Benjamin\Models\Country;
+
+class Ebanx_Gateway_Model_Mexico_Debitcard extends Ebanx_Gateway_Model_Payment_Debitcard
 {
     protected $_code = 'ebanx_dc_mx';
 
     protected $_formBlockType = 'ebanx/form_debitcard';
     protected $_infoBlockType = 'ebanx/info_debitcard';
-
-    /**
-     * Ebanx_Gateway_Model_Mexico_Debitcard constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $this->gateway = $this->ebanx->debitCard();
-    }
 
     /**
      * @param null $quote unused
@@ -27,21 +20,10 @@ class Ebanx_Gateway_Model_Mexico_Debitcard extends Ebanx_Gateway_Payment
     }
 
     /**
-     * @return void
+     * @return string
      */
-    public function setupData()
+    protected function getCountry()
     {
-        parent::setupData();
-
-        $this->data->setGatewayFields(Mage::app()->getRequest()->getPost('payment'));
-        $this->data->setPaymentType('dc');
-    }
-
-    /**
-     * @return void
-     */
-    public function transformPaymentData()
-    {
-        $this->paymentData = $this->adapter->transformCard($this->data);
+        return Country::MEXICO;
     }
 }
