8bea87f995db38172858c978d1e8e2d710226123
João Vitor Moreira <joao.moreira@ebanx.com>
2020-07-16 16:16:30 -0300

    Adapt Debitcard info to support multiple countries

--- app/code/community/Ebanx/Gateway/Block/Info/Debitcardmx.php
+++ app/code/community/Ebanx/Gateway/Block/Info/Debitcardmx.php
@@ -1,14 +1,14 @@
 <?php
 
-class Ebanx_Gateway_Block_Info_Debitcard extends Ebanx_Gateway_Block_Info_Abstract
+class Ebanx_Gateway_Block_Info_Debitcardmx extends Ebanx_Gateway_Block_Info_Abstract
 {
     /**
-     * @return Ebanx_Gateway_Block_Info_Debitcard
+     * @return Ebanx_Gateway_Block_Info_Debitcardmx
      */
     protected function _construct()
     {
         parent::_construct();
-        $this->setTemplate('ebanx/info/debitcard.phtml');
+        $this->setTemplate('ebanx/info/debitcard_mx.phtml');
         if ($this->isAdmin()) {
             $this->setTemplate('ebanx/info/default.phtml');
         }
